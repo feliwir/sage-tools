2a170e826117b1ed496c0f212c11fd2d3260830e
Stephan Vedder <stephan.vedder@gmail.com>
2017-03-04 17:49:24 +0100

    init commit

---
+++ bigedit/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using sage.big;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace bigedit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                BigArchive arch = new BigArchive(File.Open(ofd.FileName, FileMode.Open));
+
+                foreach(var entry in arch.Entries)
+                {
+                    Path.GetFullPath(entry.FullName);
+                    TreeViewItem tvi = new TreeViewItem();
+                    tvi.Header = entry.FullName;
+                    ui_treeView.Items.Add(tvi);
+                }
+            }
+        }
+    }
+}
