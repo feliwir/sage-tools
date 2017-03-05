995fae9540e7ab4864976af3c8242488fd73b19a
Stephan Vedder <stephan.vedder@gmail.com>
2017-03-05 15:19:59 +0100

    Add TextBox

--- bigedit/MainWindow.xaml.cs
+++ bigedit/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.Win32;
 using sage.big;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace bigedit
 {
@@ -13,11 +16,17 @@ namespace bigedit
     /// </summary>
     public partial class MainWindow : Window
     {
-        ViewModel m_archiveTree;
-
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(ui_filter.Text))
+                return true;
+            else
+                return ((item as BigArchiveEntry).FullName.IndexOf(ui_filter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
         }
 
         private void Open_Click(object sender, RoutedEventArgs e)
@@ -27,57 +36,53 @@ namespace bigedit
             if (ofd.ShowDialog() == true)
             {
                 BigArchive arch = new BigArchive(File.Open(ofd.FileName, FileMode.Open));
-
+                ui_listview.ItemsSource = arch.Entries;
+                CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ui_listview.ItemsSource);
+                view.Filter = UserFilter;
             }
-        }   
-    }
-
-    public interface IFolder
-    {
-        string FullPath { get; }
-        string FolderLabel { get; }
-        List<IFolder> Folders { get; }
-    }
-
-    public class Folder : IFolder
-    {
-        public List<IFolder> Folders { get; set; }
-        public string FolderLabel { get; set; }
-        public string FullPath { get; set; }
-
-        public Folder()
-        {
-            Folders = new List<IFolder>();
-        }
-    }
-
-    public class ViewModel : INotifyPropertyChanged
-    {
-        public ViewModel()
-        {
-            m_folders = new List<IFolder>();
-
-          //somehow put BigArchive inside the ViewModel
-            
         }
 
-        private List<IFolder> m_folders;
-        public List<IFolder> Folders
+        private void ui_listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            get { return m_folders; }
-            set
+            BigArchiveEntry entry = ui_listview.SelectedItem as BigArchiveEntry;
+            string extension = Path.GetExtension(entry.FullName);
+           
+            if(extension==".tga"|| extension == ".jpg" || extension == ".png")
             {
-                m_folders = value;
-                NotifiyPropertyChanged("Folders");
+                SetImage();
             }
+            else
+            {
+                SetText();
+            }
+
         }
 
-        void NotifiyPropertyChanged(string property)
+        private void SetImage()
         {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            ui_content.Children.Clear();
+            BigArchiveEntry entry = ui_listview.SelectedItem as BigArchiveEntry;
+            Image img = new Image();
+            img.Source = BitmapFrame.Create(entry.Open(), 
+                                            BitmapCreateOptions.None,
+                                            BitmapCacheOption.OnLoad);
+            ui_content.Children.Add(img);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private void SetText()
+        {
+            ui_content.Children.Clear();
+            BigArchiveEntry entry = ui_listview.SelectedItem as BigArchiveEntry;
+            StreamReader sr = new StreamReader(entry.Open());
+            TextBlock tb = new TextBlock();
+            tb.Text = sr.ReadToEnd();
+            ui_content.Children.Add(tb);
+        }
+
+        private void ui_filter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(ui_listview.ItemsSource).Refresh();
+        }
     }
+   
 }
